53f95679ecba1f1748c5752a4ace2e983920e452
RouxRhett <nsch.4871@gmail.com>
2021-11-15 16:04:16 +0900

    [Finish]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3699cdcfaae18cc37c6b0a63cffa8699bc638462da8cb42a92e1408c041fd912bbf71945ccc741344684bead07414f6d6c31c65f05214be9dd3216e26dc10364'

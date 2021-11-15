15d12fa8d630e355dfda3e780872ab548a77687f
RouxRhett <nsch.4871@gmail.com>
2021-11-15 15:31:36 +0900

    [Finish]2-6

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get 'todolists' => 'todolists#index'
   # indexに表示された投稿データの詳細を表示するためのページ
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
+  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
+  patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
 end
